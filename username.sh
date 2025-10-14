9e11fee30774ed1920cfe4f17921f54dc4841a04
mikaylasypinero <msypinero@chapman.edu>
2025-10-14 16:44:22 -0700

    Fix ShellCheck warning in username.sh

--- username.sh
+++ username.sh
@@ -9,7 +9,7 @@ echo "3. Must be 3–12 characters long."
 echo ""
 
 while true; do
-    read -p "Enter a username: " username
+    read -r  -p "Enter a username: " username
     if [[ $username =~ ^[a-z][a-z0-9_]{2,11}$ ]]; then
         echo "Thank you! '$username' is a valid username."
         break
